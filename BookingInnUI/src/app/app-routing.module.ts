4c9215aebaa81ce6ed4724a0368528daa3894a87
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-02-02 13:20:52 +0600

    router link attach in admin dashboard

--- BookingInnUI/src/app/app-routing.module.ts
+++ BookingInnUI/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { AvailableRoomComponent } from './user/available-room/available-room.component';
 import { UserIndexComponent } from './user/user-index/user-index.component';
 
 
 const routes: Routes = [
 {path:'', component:UserIndexComponent},
-{path:'availableroom/:id', component:AvailableRoomComponent}
+{path:'availableroom/:id', component:AvailableRoomComponent},
+{path: 'admindash', component:AdminDashboardComponent}
 
 
 ];
